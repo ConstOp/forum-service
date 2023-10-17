d77dcd082e5b7a4549a0f0d2aae470787f014566
costa <oparichev.k@gmail.com>
2023-10-17 19:09:15 +0300

    CustomWebSecurity

--- src/main/java/telran/java48/security/AuthorizationConfiguration.java
+++ src/main/java/telran/java48/security/AuthorizationConfiguration.java
@@ -1,10 +1,13 @@
 package telran.java48.security;
 
+import javax.websocket.Session;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
@@ -18,15 +21,26 @@ public class AuthorizationConfiguration {
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.httpBasic(Customizer.withDefaults());
 		http.csrf(csrf -> csrf.disable());
-		http.authorizeRequests((authorize) -> authorize.mvcMatchers("/account/register", "/forum/posts/**").permitAll()
-				.mvcMatchers("/account/user/{login}/role/{role}").hasRole("ADMINISTRATOR")
-				.mvcMatchers(HttpMethod.PUT, "/account/user/{login}").access("#login == authentication.name")
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.authorizeRequests((authorize) -> authorize
+				.mvcMatchers("/account/register", "/forum/posts/**")
+				.permitAll()
+				.mvcMatchers("/account/user/{login}/role/{role}")
+				.hasRole("ADMINISTRATOR")
+				.mvcMatchers(HttpMethod.PUT, "/account/user/{login}")
+				.access("#login == authentication.name")
 				.mvcMatchers(HttpMethod.DELETE, "/account/user/{login}")
 				.access("#login == authentication.name or hasRole('ADMINISTRATOR')")
-				.mvcMatchers("/forum/post/{author}", "/forum/post/{id}/comment/{author}")
-				.access("#author == authentication.name").anyRequest().authenticated());
-//				.addFilterBefore(new DeletePostFilter(), AuthorizationFilter.class)
-//				.addFilterBefore(new UpdatePostFilter(), AuthorizationFilter.class);
+				.mvcMatchers(HttpMethod.POST, "/forum/post/{author}", "/forum/post/{id}/comment/{author}")
+				.access("#author == authentication.name")
+				.mvcMatchers(HttpMethod.PUT,"/forum/post/{id}/comment/{author}")
+				.access("#author == authentication.name")
+				.mvcMatchers(HttpMethod.PUT, "/forum/post/{id}")
+				.access("customSecurity.checkPostAuthor(#id, authentication.name)")
+				.mvcMatchers(HttpMethod.DELETE, "/forum/post/{id}")
+				.access("customSecurity.checkPostAuthor(#id, authentication.name) or hasRole('MODERATOR')")
+				.anyRequest()
+				.authenticated());
 		return http.build();
 	}
 
--- src/main/java/telran/java48/security/UserDelailsServiceImpl.java
+++ src/main/java/telran/java48/security/UserDelailsServiceImpl.java
@@ -13,18 +13,14 @@ import telran.java48.accounting.model.UserAccount;
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailsServiceImpl implements UserDetailsService {
-	
+public class UserDelailsServiceImpl implements UserDetailsService {
 	final UserAccountRepository userAccountRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		UserAccount userAccount = userAccountRepository.findById(username)
-				.orElseThrow(()-> new UsernameNotFoundException(username));
-		String []roles = userAccount.getRoles()
-				.stream()
-				.map(r-> "ROLE_"+ r.toUpperCase())
-				.toArray(String[]::new);
+				.orElseThrow(() -> new UsernameNotFoundException(username));
+		String[] roles = userAccount.getRoles().stream().map(r -> "ROLE_" + r.toUpperCase()).toArray(String[]::new);
 		return new User(username, userAccount.getPassword(), AuthorityUtils.createAuthorityList(roles));
 	}
 
